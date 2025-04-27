7cb98340beda5be02591187dbbad5f46fd3339e4
alekenzy <alekenzy.job@gmail.com>
2025-04-28 01:11:05 +0500

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,35 @@
+import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
+
+@Controller('api')
+export class AppController {
+  @Get()
+  getHello(): string {
+    return '<h1>Hello World!</h1>' + 'Atak Isteid Na';
+  }
+
+  @Post('messages') async messages(
+    @Body('username') username: string,
+    @Body('message') message: string,
+  ): Promise<{
+    status: number
+    message: string
+    data: {
+      username: string
+      message: string
+    }
+  }> {
+
+    if (!username || !message) {
+      throw new BadRequestException('Not all data passed', {
+        cause: new Error(),
+        description: 'Username or message is missing',
+      });
+    }
+
+    return {
+      status: 200,
+      message: 'success',
+      data: { username, message }
+    };
+  }
+}
